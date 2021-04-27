7fb21e213d4d8219e05b1a93f2e246ce82e48fe9
Luke Oloffs <luke@Lukes-MacBook-Air.local>
2021-04-27 14:43:38 -0700

    Palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,34 @@
-const isPalindrome = function(string) {
-  string = string.toLowerCase()
-  let reverse = string.split('').reverse().join('')
-  if (string === reverse) {
-    return true;
+// const { truncate } = require("node:fs");
+
+// const isPalindrome = function(string) {
+//   string = string.toLowerCase()
+//   let reverse = string.split('').reverse().join('')
+//   if (string === reverse) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// console.log(isPalindrome('Anna'))
+
+
+
+
+
+
+
+
+
+
+
+const palindrome = function(word) {
+  word = word.toString().toLowerCase();
+  const reversedWord = word.split('').reverse().join('')
+  if (reversedWord === word) {
+    return true
   }
-  return false;
+  return false
 }
 
-console.log(isPalindrome('Anna'))
+
+console.log(palindrome("Anna"))
