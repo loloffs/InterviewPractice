7c1b8f2b8d06f4fe0f25ee2e2044eea9b3afb321
loloffs <lukeoloffs@gmail.com>
2021-07-27 15:30:02 -0700

    Practice

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -24,22 +24,60 @@
 
 
 
-
-
-
-
-const fizzbuzz = function(range, multiples, words) {
+const fizzbuzz = function(range, words, multiples) {
   for (let i = range[0]; i <= range[1]; i++) {
     if (i % multiples[0] === 0 && i % multiples[1] === 0) {
       console.log(`${words[0]}${words[1]}`);
-    } else if (i % multiples[1] === 0) {
-      console.log(words[1]);
     } else if (i % multiples[0] === 0) {
       console.log(words[0]);
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
     } else {
       console.log(i);
     }
   }
 };
 
-fizzbuzz([25, 150], [4, 6], ['test', 'words']);
+fizzbuzz([50, 200], ['fizz', 'buzz'], [10, 15]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fizzbuzz = function(range, multiples, words) {
+//   for (let i = range[0]; i <= range[1]; i++) {
+//     if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+//       console.log(`${words[0]}${words[1]}`);
+//     } else if (i % multiples[1] === 0) {
+//       console.log(words[1]);
+//     } else if (i % multiples[0] === 0) {
+//       console.log(words[0]);
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// };
+
+// fizzbuzz([25, 150], [4, 6], ['test', 'words']);
